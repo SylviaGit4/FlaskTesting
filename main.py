ad732e40cfba3e84e5212657455d3715ed49f92a
SylviaGit4 <torchicky139@gmail.com>
2025-10-14 15:59:18 +0100

    Forms Testing

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
 
-name = "user"
+class NameForm(FlaskForm):
+    name = StringField("Username: ", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 items = ["Item 1", "Item 2", "Item 3"]
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name, items = items)
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name = name)
-
-@app.route('/list')
-def list():
-    return render_template('list.html', items = items)
 
 @app.errorhandler(404)
 def page_not_found(e):
