c44f8926f7e1757c8ef1d36b9070a9ac395947c4
SylviaGit4 <torchicky139@gmail.com>
2025-10-14 15:10:33 +0100

    List.html

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 name = "user"
+items = ["Item 1", "Item 2", "Item 3"]
 
 @app.route('/')
 def index():
@@ -11,6 +12,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name = name)
+
+@app.route('/list')
+def list():
+    return render_template('list.html', items = items)
 
 @app.errorhandler(404)
 def page_not_found(e):
