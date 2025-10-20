652b3f1addecdb8ca949dc3ab52aed221b5783b2
SylviaGit4 <torchicky139@gmail.com>
2025-10-20 16:53:20 +0100

    WIP Database stuff

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, session
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+import os
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir + 'data.sqlite')
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField("Username: ", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+
+with app.app_context():
+    db.create_all()
 
 items = ["Item 1", "Item 2", "Item 3"]
 
